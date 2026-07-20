9ef48b8137f73879bd51b45c1592515def39d4d8
Ryan J Morrow <ryan-morrow@uiowa.edu>
2026-07-20 12:56:11 -0500

    Update navigation v01

--- app.py
+++ app.py
@@ -335,7 +335,13 @@ with st.sidebar:
                 st.session_state.page = "Approve Users"
 
             if st.button("Add Diver"):
+                st.session_state.page = "Add Meet"
+            if st.button("Add Diver"):
                 st.session_state.page = "Add Diver"
+            if st.button("Add Diver"):
+                st.session_state.page = "Add Results"
+            if st.button("Add Diver"):
+                st.session_state.page = "View/Edit Results"
 
 page = st.session_state.page
 
