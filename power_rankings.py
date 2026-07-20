3055c3d3ca83f4c78a53dda1ddd37d2c75eed37c
Ryan J Morrow <ryan-morrow@uiowa.edu>
2026-07-19 22:28:49 -0500

    Power Rankings Adjustment v10

--- power_rankings.py
+++ power_rankings.py
@@ -454,8 +454,8 @@ def render_power_rankings_page(supabase):
         for col in [c for c in grid_df.columns if c.endswith("_tooltip")]:
             gb.configure_column(col, hide=True)
 
-        grid_options=gb.build(),
-        grid_options["enableBrowserTooltips"] = True,
+        grid_options=gb.build()
+        grid_options["enableBrowserTooltips"] = True
 
         AgGrid(
             grid_df,
