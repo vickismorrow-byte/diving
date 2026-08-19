3954f4d8bf39a723d0b112eaf8c68ac8144db798
Ryan J Morrow <ryan-morrow@uiowa.edu>
2026-08-19 10:27:00 -0400

    Add List Functionality v08

--- edit_lists.py
+++ edit_lists.py
@@ -174,9 +174,18 @@ def render_edit_lists_page(supabase):
     df = pd.DataFrame(rows)
 
     st.subheader("Current List")
+    st.caption('When editing the list, only change Dive Numbers, the DD will change automatically when you click "Save Changes"')
+
+    styled_df = df.style.set_properties(
+        subset=["Voluntary DD", "Optional DD"],
+        **{
+            "background-color": "#f0f0f0",
+            "color": "#888888",
+        }
+    )
 
     edited_df = st.data_editor(
-        df,
+        styled_df,
         key="lists_editor",
         hide_index=True,
         use_container_width=True,
